6d91a5475c21a2802450d8e5e1fffdb345babfff
Jesse <laethaka@gmail.com>
2018-05-17 14:01:18 -0700

    initial files

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var keys = require('keys.js');
+
+var spotify = new Spotify(keys.spotify);
+var twitter = new Twitter(keys.twitter);
+
+var userInput = process.argv[2];
